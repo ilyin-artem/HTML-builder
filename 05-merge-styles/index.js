f23b68abc293ff4157bf70ad44180441c92f952a
Artyom Ilyin <ilyin@infostorm.ru>
2022-05-24 01:29:16 +0300

    feat: 05-merge styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const fs = require('fs/promises');
+
+const sourcePath = path.join(__dirname, '/styles');
+const destinationPath = path.join(__dirname, '/project-dist', 'bundle.css');
+
+async function mergeStyles(source, destBundle) {
+    try {
+        let cssData = '';
+        const sourceElements = await fs.readdir(source, {
+            withFileTypes: true,
+        });
+
+        for (const element of sourceElements) {
+            if (element.isFile() && path.extname(element.name) === '.css') {
+                const cssPath = path.join(sourcePath, element.name);
+
+                // cssData += cssData === '' ? '' : '\n';
+                cssData += (await fs.readFile(cssPath, 'utf8')) + '\n';
+            }
+        }
+        await fs.writeFile(destBundle, cssData, 'utf8');
+    } catch (e) {
+        console.log(`Error: ${e.message}`);
+    }
+}
+
+mergeStyles(sourcePath, destinationPath);
